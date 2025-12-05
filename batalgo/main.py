598f7a8768c81fae55391225e643b9511697e0b7
Alexandros E. Ntigkaris <87975862+ntigkaris@users.noreply.github.com>
2025-12-05 11:46:48 +0200

    Update main.py

--- batalgo/main.py
+++ batalgo/main.py
@@ -3,24 +3,24 @@ author:     Alexandros E. Ntigkaris
 python:     3.9.2
 numpy:      1.19.5
 matplotlib: 3.3.4
+logging:    0.4.9.6
 '''
 
+from utils import BatColony
+
+params = {
+            'entities':15,
+            'timesteps':10,
+            'alpha':0.9,
+            'gamma':0.9,
+            'benchmark_fn':'dejong',
+            'random_state':2036,
+            'sleep_rate':1e-10,
+            'verbose':True,
+        }
+
 if __name__ == '__main__':
-
-    from utils import BatColony
-
-    params = {
-                'entities':15,
-                'timesteps':10,
-                'alpha':0.9,
-                'gamma':0.9,
-                'benchmark_fn':'dejong',
-                'random_state':2036,
-                'sleep_rate':1e-10,
-                'verbose':True,
-            }
-
     bats = BatColony(**params)
     bats.fill()
     bats.run()
-
+    
